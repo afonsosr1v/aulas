45af885356caa9ba67ab19b89881b854942bf1e1
Afonso Nóbrega <77923826+afonsosr1v@users.noreply.github.com>
2023-03-13 13:40:59 +0000

    Add files via upload

--- TS/aula2.m
+++ TS/aula2.m
@@ -1,1 +1,23 @@
-l@Á¤“@ò@l%%†¤•ƒ£‰–•@§~„…‡M]%@@§~ñ\Mnð]^%…•„%%†¤•ƒ£‰–•@§~‰”—M]%@@§~ñ\M~ð]^%…•„%%£‰”…„–”‰•@~@`ñðzñð^%%¢‰‡•“@~@ò\‰”—M£‰”…„–”‰•Nø]@N@„…‡M£‰”…„–”‰•Nö]^%¢‰‡•“@~@¢‰‡•“@N@‰”—M£‰”…„–”‰•Nô]@N@‰”—M£‰”…„–”‰•Nñ]@N@‰”—M£‰”…„–”‰•]^%¢‰‡•“@~@¢‰‡•“@N@‰”—M£‰”…„–”‰•`ñ]@`@‰”—M£‰”…„–”‰•`õ]^%%¢£…”M¢‰‡•“k@£‰”…„–”‰•]%£‰£“…M}Å§…”—“–@ñK}]%§“‚…“M}£‰”…„–”‰•}]%¨“‚…“M}¢‰‡•“M£‰”…„–”‰•}]%%%
+%%aula 2
+
+function x=deg(a)
+  x=1*(a>0);
+end
+
+function x=imp(a)
+  x=1*(a==0);
+end
+
+timedomain = -10:10;
+
+signal = 2*imp(timedomain+8) + 1*deg(timedomain+6);
+signal = signal + 1*imp(timedomain+4) + 1*imp(timedomain+1) + 1*imp(timedomain);
+signal = signal + 1*imp(timedomain-1) - 1*imp(timedomain-5);
+
+stem(timedomain, signal)
+title('Exemplo 1.a')
+xlabel('timedomain')
+ylabel('signal(timedomain')
+
+
+
